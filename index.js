cddd0cfdfda3a232f9dd224d6035b8ea9012f935
apajitnov <alexei@pajitnov.info>
2017-10-14 11:50:32 -0400

    refactoring of the entry file

--- index.js
+++ index.js
@@ -38,3 +38,47 @@ logger.error('Unhandled Rejection at: Promise ', p, reason)
 server.on('listening', () =>
 logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
 );
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
+process.on('unhandledRejection', (reason, p) =>
+logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
+process.on('unhandledRejection', (reason, p) =>
+logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
+process.on('unhandledRejection', (reason, p) =>
+logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
+process.on('unhandledRejection', (reason, p) =>
+logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
+
+server.on('listening', () =>
+logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
