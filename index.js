290ae1590b39f89976d7ad3f4d616c01467a7b04
apajitnov <alexei@pajitnov.info>
2017-10-14 11:53:55 -0400

    entry point update

--- index.js
+++ index.js
@@ -9,14 +9,3 @@ logger.error('Unhandled Rejection at: Promise ', p, reason)
 server.on('listening', () =>
 logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
 );
-
-server.on('listening', () =>
-logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
-process.on('unhandledRejection', (reason, p) =>
-logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
-
-server.on('listening', () =>
-logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
