a8c115cba7370739a5ecedc08b1553630384d104
apajitnov <alexei@pajitnov.info>
2017-10-13 21:53:41 -0400

    merging migration changes to master

---
+++ services/node_modules/feathers-sequelize/lib/hooks/hydrate.js
@@ -0,0 +1,59 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var factory = function factory(Model) {
+  var include = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+  return function (item) {
+    // (Darren): We have to check that the Model.Instance static property exists
+    // first since it's been deprecated in Sequelize 4.x.
+    // See: http://docs.sequelizejs.com/manual/tutorial/upgrade-to-v4.html
+    var shouldBuild = Model.Instance ? !(item instanceof Model.Instance) : !(item instanceof Model);
+
+    if (shouldBuild) {
+      return Model.build(item, { isNewRecord: false, include: include });
+    }
+
+    return item;
+  };
+};
+
+exports.default = function (options) {
+  options = options || {};
+
+  return function (hook) {
+    if (hook.type !== 'after') {
+      throw new Error('feathers-sequelize hydrate() - should only be used as an "after" hook');
+    }
+
+    var makeInstance = factory(this.Model, options.include);
+    switch (hook.method) {
+      case 'find':
+        if (hook.result.data) {
+          hook.result.data = hook.result.data.map(makeInstance);
+        } else {
+          hook.result = hook.result.map(makeInstance);
+        }
+        break;
+
+      case 'get':
+      case 'update':
+        hook.result = makeInstance(hook.result);
+        break;
+
+      case 'create':
+      case 'patch':
+        if (Array.isArray(hook.result)) {
+          hook.result = hook.result.map(makeInstance);
+        } else {
+          hook.result = makeInstance(hook.result);
+        }
+        break;
+    }
+    return Promise.resolve(hook);
+  };
+};
+
+module.exports = exports['default'];
