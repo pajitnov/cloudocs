eb2beee13bd21c49c3f6687a5bd9e6cf6662ffdf
apajitnov <alexei@pajitnov.info>
2017-10-16 15:01:53 -0400

    db settings updated

--- nodemysql/app.js
+++ nodemysql/app.js
@@ -1,12 +1,21 @@
+// npm init
+// npm install --save mysql express
+// touch app.js
+//
+//
+//
+
 const express = require('express');
 const mysql = require('mysql');
+
+
 
 // Create connection
 const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '123456',
-    database : 'nodemysql'
+    database : 'cloudocs'
 });
 
 // Connect
