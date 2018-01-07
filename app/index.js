4f2a27ba8b7459513f8b8855f0e0ff6a1096f0a3
apajitnov <alexei@pajitnov.info>
2018-01-07 14:57:05 -0500

    application files

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const messages = require('./messages/messages.service.js');
+module.exports = function () {
+    const app = this; // eslint-disable-line no-unused-vars
+    app.configure(messages);
+
+
+
+    //content
+};
